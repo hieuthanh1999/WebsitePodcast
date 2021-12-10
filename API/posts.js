edc0ed0b5ddab17b50b3a79eef2dd7bb05727d50
hieuthanh1999 <n.hieuthanhps@gmail.com>
2021-12-10 23:13:42 +0700

    update

---
+++ API/posts.js
@@ -0,0 +1,40 @@
+var url = 'http://localhost:8000/post';
+fetch(url)
+    .then(response => response.json())
+    .then(function (responses) {
+       
+       var htmls = responses.map(function (response) {
+            return  `
+            <div class="itemhome__post">
+            <div class="item__img">
+              <img src="${response.image}" alt="">
+            </div>
+            <div class="item__info">
+              <div class="info__ava">
+                <img src="../../IMG/logo_vn.png" alt="">
+              </div>
+              <div class="info__text">
+                <h5>${response.user}</h5>
+                <p class="realtime">4 giờ trước</p>
+              </div>
+              <div style="position: relative; top: -6px; left: 5px; font-size: 10px; cursor: pointer;">
+                <i class="fas fa-crown" title="Quản trị viên"></i>
+              </div>
+            </div>
+            <div class="item__title">
+              <h3>${response.title}</h3>
+            </div>
+            <div style="border-bottom: 1px solid#ccc; width: 250px; margin: auto;"></div>
+            <div class="item__icon">
+              <i class="far fa-eye">${response.numview}</i>
+              <i class="far fa-heart">${response.numlove}</i>
+            </div>
+          </div>
+            `;
+       });
+       var html = htmls.join('');
+       document.getElementById('postItem').innerHTML = html;
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
--- API/register.js
+++ API/register.js
@@ -1,20 +1,33 @@
-var getUrl = window.location;
-var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+// var getUrl = window.location;
+// var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
 
-var url2= baseUrl + "/API/user/read";
+var url2= 'http://localhost:8000/users';
 
-var url=  baseUrl + "/API/user/create";
+var url=  'http://localhost:8000/user';
 // if(getData() == true){
 //   setForm();
 // }else{
 //   console.log("failed");
 // }
 function setData(data) {
+  
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Access-Control-Allow-Origin','*')
+  myHeaders.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS')
+  myHeaders.append('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+
+  VALUE = JSON.stringify(data, null, 2);
     var options ={
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
+      method: 'POST',
+      headers: myHeaders,
+      mode:"cors",
+      cache: 'default',
+      body: VALUE,
+      Credential:false,
     }
+
+
     fetch(url, options)
       .then(response => response.json())
       .then(data => {
@@ -64,9 +77,45 @@ function setForm(){
             "ranker": null
         }
         setData(formData);
-        window.location.href = "login.html"
+        // window.location.href = "login.html"
          
     })
 }
 
 setForm();
+
+
+// function submitInfor() {
+//   var postApi = "http://localhost:8080/authenticate"
+// let form = document.forms["myForm"];
+// let fd = new FormData(form);
+// let data = {};
+// for (let [key, prop] of fd) {
+//   data[key] = prop;
+// }
+// VALUE = JSON.stringify(data, null, 2);
+
+// const myHeaders = new Headers();
+// myHeaders.append('Content-Type', 'application/json');
+// myHeaders.append('Access-Control-Allow-Origin','*')
+// myHeaders.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS')
+// myHeaders.append('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+
+
+// fetch(postApi, {
+//   method: 'POST',
+//   headers: myHeaders,
+//   mode:"cors",
+//   cache: 'default',
+//   body: VALUE,
+//   Credential:false,
+// })
+//   .then(data => data.json())
+//   .then(data => { console.log(data)
+//     localStorage.setItem("token",data.jwtToken)
+//     window.location.href = "WebsitePodcast/FE/SRC/HTML/homepage.html"
+//    })
+//   .catch((err) => {
+//     console.error(err);
+//   })
+// }
