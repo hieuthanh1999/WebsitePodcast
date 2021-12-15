c872e75725d2077aefbb5883fb0e1de6ecb58227
hieuthanh1999 <n.hieuthanhps@gmail.com>
2021-12-15 22:37:54 +0700

    push

--- API/register.js
+++ API/register.js
@@ -1,121 +1,40 @@
-// var getUrl = window.location;
-// var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+var postApi = "http://localhost:8000/user";
+form.addEventListener('submit', function (e) {
+  let username = document.getElementById('username').value;
+  let email = document.getElementById('email').value;
+  let phoneNumber = document.getElementById('phone').value;
+  let pass = document.getElementById('pass').value;
+  var getUrl = window.location;
+  var localhost = getUrl.protocol + "//" + getUrl.host + "/";
+  var datas = {
+    "name": username,
+    "email": email,
+    "password": pass,
+    "phone": phoneNumber,
+    "avatar": localhost+ "WebsitePodcast/IMG/user.jpg",
+    "type": "user",
+    "ranker": "đồng"
+  }
+  VALUE = JSON.stringify(datas);
 
-var url2= 'http://localhost:8000/users';
-
-var url=  'http://localhost:8000/user';
-// if(getData() == true){
-//   setForm();
-// }else{
-//   console.log("failed");
-// }
-function setData(data) {
-  
   const myHeaders = new Headers();
   myHeaders.append('Content-Type', 'application/json');
-  myHeaders.append('Access-Control-Allow-Origin','*')
-  myHeaders.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS')
-  myHeaders.append('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+  myHeaders.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  myHeaders.append('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+  myHeaders.append('Access-Control-Allow-Origin', '*');
+  myHeaders.append('Access-Control-Allow-Credentials', 'true');
 
-  VALUE = JSON.stringify(data, null, 2);
-    var options ={
-      method: 'POST',
-      headers: myHeaders,
-      mode:"cors",
-      cache: 'default',
-      body: VALUE,
-      Credential:false,
-    }
-
-
-    fetch(url, options)
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-}
-
-// function getData() {
-//   let email = document.getElementById('email').value;
-//     fetch(url2)
-//       .then(response => response.json())
-//       .then(data => {
-//         data['user'].forEach(element => {
-//             if(email == element.email){
-//                 console.log("đã có email");
-//                 return false;
-//             }else{
-//                 return true;
-//             }
-//         })
-//         })
-//       .catch((error) => {
-//         console.error('Error:', error);
-//       });
-// }
-function setForm(){
-    var btn_account = document.querySelector('#register_account');
-    form.addEventListener('submit', function (e) {
-      // e.preventDefault()
-      // checkInputs()
-      // alert("Dsadsa");
-      let username = document.getElementById('username').value;
-      let email = document.getElementById('email').value;
-      let phoneNumber = document.getElementById('phone').value;
-      let pass = document.getElementById('pass').value;
-        var formData =
-        {
-            "name": username,
-            "email": email,
-            "password": pass,
-            "phone": phoneNumber,
-            "avatar": null,
-            "type": null,
-            "ranker": null
-        }
-        setData(formData);
-        // window.location.href = "login.html"
-         
+  fetch(postApi, {
+    method: 'POST',
+    headers: myHeaders,
+    mode: 'no-cors',
+    body: VALUE
+  })
+    .then(data => {
+      console.log(data);
+      window.location.href = "login.html"
     })
-}
-
-setForm();
-
-
-// function submitInfor() {
-//   var postApi = "http://localhost:8080/authenticate"
-// let form = document.forms["myForm"];
-// let fd = new FormData(form);
-// let data = {};
-// for (let [key, prop] of fd) {
-//   data[key] = prop;
-// }
-// VALUE = JSON.stringify(data, null, 2);
-
-// const myHeaders = new Headers();
-// myHeaders.append('Content-Type', 'application/json');
-// myHeaders.append('Access-Control-Allow-Origin','*')
-// myHeaders.append('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS')
-// myHeaders.append('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
-
-
-// fetch(postApi, {
-//   method: 'POST',
-//   headers: myHeaders,
-//   mode:"cors",
-//   cache: 'default',
-//   body: VALUE,
-//   Credential:false,
-// })
-//   .then(data => data.json())
-//   .then(data => { console.log(data)
-//     localStorage.setItem("token",data.jwtToken)
-//     window.location.href = "WebsitePodcast/FE/SRC/HTML/homepage.html"
-//    })
-//   .catch((err) => {
-//     console.error(err);
-//   })
-// }
+    .catch((err) => {
+      console.error(err);
+    });
+});
--- API/url_home.js
+++ API/url_home.js
@@ -1,19 +1,31 @@
-var url_podcast = 'http://localhost:8000/post';
+var url_blog = 'http://localhost:8000/post';
 var url_category_pod = 'http://localhost:8000/category';
+var url_podcast = 'http://localhost:8000/podcast';
 
 //Xử lý session user
 let name = sessionStorage.getItem('user');
+let avatar = sessionStorage.getItem('avatar');
+if(name){
+  function course_click(id){
+  sessionStorage.setItem('id-course', id);
+  window.location.href = "course_detail.html";
+}
+}
 if(name != null){
   console.log(name);
+  console.log(avatar);
   document.getElementById('nonelogin').style.display = 'none';
    document.getElementById('logout').style.display = 'block';
   document.getElementById('name-profile').innerHTML = 'Hi: '  + name;
   document.getElementById('name-profile').style.color = 'white';
+  
+  document.getElementById('profile').src = avatar;
   document.getElementById('profile').style.display = 'block';
   
 }
 else{
   console.log(name);
+  console.log(avatar);
   document.getElementById('logout').style.display = 'none';
   document.getElementById('nonelogin').style.display = 'flex';
   document.getElementById('name-profile');
@@ -21,5 +33,7 @@ else{
 var logout =  document.getElementById('logouthome');
 logout.onclick = function(){
   console.log(name);
+  console.log(avatar);
   sessionStorage.removeItem('user');
+  sessionStorage.removeItem('avatar');
 }
